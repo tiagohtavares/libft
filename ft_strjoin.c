78dd5b1d1e8b7aa23f85da3ed89976899bb2081a
Tiago Henrique Tavares <ttavares@c1r12s16.42porto.com>
2023-03-30 11:20:11 +0100

    get_next_line fix

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,11 +6,7 @@
 /*   By: ttavares <ttavares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 17:46:36 by ttavares          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/03/30 11:15:46 by ttavares         ###   ########.fr       */
-=======
-/*   Updated: 2023/03/26 02:18:38 by ttavares         ###   ########.fr       */
->>>>>>> 116652ca5d74d441d8c31f3aaa3ad55c5cd37682
+/*   Updated: 2023/03/30 11:19:14 by ttavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +16,11 @@ char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*ptr;
 
-<<<<<<< HEAD
 	if (!s1)
 	{
 		s1 = (char *)malloc(1 * sizeof(char));
 		s1[0] = '\0';
 	}
-=======
->>>>>>> 116652ca5d74d441d8c31f3aaa3ad55c5cd37682
 	if (!s1 || !s2)
 		return (0);
 	ptr = malloc(sizeof(char) *(ft_strlen(s1) + ft_strlen(s2) + 1));
